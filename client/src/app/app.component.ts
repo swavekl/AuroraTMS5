ab0cc8078f28089d1d53da343f0820b32f0bd0f0
Swavek <swaveklorenc@yahoo.com>
2017-09-18 20:02:19 -0500

    Made application aware of size of screen to hide sidenav menu on mobile devices.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { TodosService, Todo } from './todos.service';
 
+import {Subscription} from "rxjs/Subscription";
+import 'rxjs/add/operator/filter';
+import {MediaChange, ObservableMedia} from "@angular/flex-layout";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,13 +14,25 @@ export class AppComponent {
   title = 'app';
   todos: Todo[];
 
-  constructor (private todosService: TodosService) {
+  watcher: Subscription;
+  activeMediaQuery = "";
+  isMobile : boolean;
 
+  constructor(media: ObservableMedia, private todosService: TodosService) {
+    // subscribe to media changes so we can detect if we are on mobile device or not
+    this.watcher = media.subscribe((change: MediaChange) => {
+      this.activeMediaQuery = change ? `'${change.mqAlias}' = (${change.mediaQuery})` : "";
+      this.isMobile = ( change ? (change.mqAlias == 'xs') : false );
+//      console.log ('this.activeMediaQuery = ', this.activeMediaQuery);
+//      console.log ('this.isMobile ', this.isMobile);
+    });
   }
 
-  ngOnInit() {
-  console.log ('in ngOnInit');
+  ngOnDestroy() {
+    this.watcher.unsubscribe();
+  }
 
+    ngOnInit() {
     this.todosService.getTodos().subscribe(
     data =>
     {
